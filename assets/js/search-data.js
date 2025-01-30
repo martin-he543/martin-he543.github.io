1987bba5e66384fb3a27221298e3206eb45380d6
martin-he543 <41821907+martin-he543@users.noreply.github.com>
2025-01-30 11:36:22 +0000

    Deploying to gh-pages from @ martin-he543/martin-he543.github.io@63a7bba799f175633d636fefa6ba89a58d6dfc0a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -89,14 +89,35 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%6D%61%72%74%69%6E.%68%65%35%34%33@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-github',
+        title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://github.com/martin-he543", "_blank");
+        },
+      },{
+        id: 'social-instagram',
+        title: 'Instagram',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://instagram.com/martin_he543", "_blank");
+        },
+      },{
+        id: 'social-lastfm',
+        title: 'Last FM',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.last.fm/user/martinhe543", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/martin-he543", "_blank");
         },
       },{
         id: 'social-rss',
@@ -106,18 +127,11 @@ ninja.data = [{
           window.open("/feed.xml", "_blank");
         },
       },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
+        id: 'social-spotify',
+        title: 'Spotify',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://open.spotify.com/user/martinhe123", "_blank");
         },
       },{
       id: 'light-theme',
